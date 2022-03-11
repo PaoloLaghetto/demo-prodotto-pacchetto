90f566aa469eebb75291145a2d4f034097fd6998
Emanuele De Fano <e.defano@hotmail.com>
2022-03-11 14:59:29 +0100

    merge - npm install primeicons primeflex quill - carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { ProductService } from './productservice';
+import { Product2 } from './product2';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo-prodotto-pacchetto';
+
+
+  products: Product2[] = [];
+	
+	responsiveOptions;
+
+	constructor(private productService: ProductService) { 
+		this.responsiveOptions = [
+            {
+                breakpoint: '1024px',
+                numVisible: 3,
+                numScroll: 3
+            },
+            {
+                breakpoint: '768px',
+                numVisible: 2,
+                numScroll: 2
+            },
+            {
+                breakpoint: '560px',
+                numVisible: 1,
+                numScroll: 1
+            }
+        ];
+	}
+
+	ngOnInit() {
+		this.productService.getProductsSmall().then(products => {
+			this.products = products;
+		});
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
 import {InputNumberModule} from "primeng/inputnumber";
-import {FormsModule} from "@angular/forms";
+
+import { ProductService } from './productservice';
+
+import {CarouselModule} from 'primeng/carousel';
+import {ButtonModule} from 'primeng/button';
+import {ToastModule} from 'primeng/toast';
 
 @NgModule({
   declarations: [
@@ -16,9 +25,16 @@ import {FormsModule} from "@angular/forms";
     BrowserModule,
     AppRoutingModule,
     InputNumberModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+
+    CarouselModule,
+    ButtonModule,
+    ToastModule,
+    HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
